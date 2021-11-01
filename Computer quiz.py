2e3b3a72798f7ef7e92ce59345a5c5c56f5f8ff3
Aaryan <72549723+aaryan-clis@users.noreply.github.com>
2021-11-01 19:43:41 +0530

    Initial

---
+++ Computer quiz.py
@@ -0,0 +1,16 @@
+print("Welcome to the computer quiz.")
+
+playing = input ("Do you want to play ? ")
+if  playing != 'yes':
+   quit()
+print ("Ok lets play :)")
+answer = input("What is capital of India ?")
+if answer =="Delhi":
+      print("correct ! ")
+      answer = input("What is 1 + 1 ? ")
+if answer =="2":
+      print("correct ! ")
+      answer = input("What is 2 * 1 ? ")
+if answer =="2":
+      print("correct ! ")
+print ("Thanks for playing computer quiz:) ")
